9c57b0a4560a9a782daf41ea6e6c5ecb65ac8d66
fethulmubin <fetihul@gmail.com>
2025-12-01 11:03:38 +0300

    schedue edited

--- src/cron/scheduler.ts
+++ src/cron/scheduler.ts
@@ -5,13 +5,13 @@ import { reportBookReaders, reportSalawat } from "../bot/report";
 const GROUP_CHAT_ID = Number(process.env.GROUP_CHAT_ID);
 
 // Daily at 8 AM
-cron.schedule("* * * * *", async () => {
+cron.schedule("0 8 * * *", async () => {
   await sendDailyPoll();
   await reportSalawat(GROUP_CHAT_ID, "daily");
 });
 
 // Weekly on Sunday at 8 AM
-cron.schedule("* * * * *", async () => {
+cron.schedule("0 8 * * 0", async () => {
 //   await sendWeeklyPoll();
   await reportBookReaders(GROUP_CHAT_ID, "weekly");
 });
