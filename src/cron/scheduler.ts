9331d019dbb99f801d7c73138e07ea4d883536b8
fethulmubin <fetihul@gmail.com>
2025-12-01 22:56:17 +0300

    fixed

--- src/cron/scheduler.ts
+++ src/cron/scheduler.ts
@@ -3,7 +3,7 @@ import { sendDailyPoll } from "../bot/polls";
 import { reportBookReaders, reportSalawat } from "../bot/report";
 
 const GROUP_CHAT_ID = Number(process.env.GROUP_CHAT_ID);
-const ENABLE_TWO_MIN_TEST = String(process.env.ENABLE_TWO_MIN_TEST || '').toLowerCase() === 'true';
+const ENABLE_TWO_MIN_TEST = true;
 
 // Ethiopia timezone (Africa/Addis_Ababa)
 const TZ = "Africa/Addis_Ababa";
